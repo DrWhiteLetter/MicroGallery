cb5d4b08301e89e3a746077c1bec95ce94773535
Tobias Kohlhepp <tk@root-shell.de>
2018-03-17 16:48:58 +0100

    init

--- class/MicroGallery.php
+++ class/MicroGallery.php
@@ -1,12 +1,25 @@
+<?php
+
 class MicroGallery {
   public $imgDir = '../img/';
   public $templdir = '../templates/';
 
   function getImages() {
-    echo "hallo Welt";
+    $images = array();
+    $dir = opendir($this->imgDir);
+    $extensions = array("jpg", "bmp", "gif", "jpeg", "png");
+
+    while(($file = readdir($dir)) !== false){
+      $fExt = pathinfo($file, PATHINFO_EXTENSION);
+      if(in_array($fExt, $extensions){
+        $images[] = $file;
+      }
+    }
+    return $images;
   }
 
   function showGallery() {
-
+    $images = $this->getImages();
+    var_dump($images);
   }
 }
