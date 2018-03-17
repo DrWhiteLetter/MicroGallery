5dce53f6fb5c139765c7a4e3c83d3e5d058697d4
Tobias Kohlhepp <tk@root-shell.de>
2018-03-17 17:35:31 +0100

    Add Bootstrap

--- class/MicroGallery.php
+++ class/MicroGallery.php
@@ -2,16 +2,16 @@
 
 class MicroGallery {
   public $imgDir = 'img/';
-  public $templdir = 'templates/';
+  public $templDir = 'templates/';
 
   function getImages() {
     $images = array();
     $dir = opendir($this->imgDir);
     $extensions = array("jpg", "bmp", "gif", "jpeg", "png");
 
-    while(($file = readdir($dir)) !== false){
+    while ( ($file = readdir($dir)) !== false ) {
       $fExt = pathinfo($file, PATHINFO_EXTENSION);
-      if(in_array($fExt, $extensions){
+      if ( in_array($fExt, $extensions) ) {
         $images[] = $file;
       }
     }
@@ -20,11 +20,11 @@ class MicroGallery {
 
   function showGallery() {
     $html = array();
-    $templateItem = file_get_contents($this->templates."imageItem.html");
+    $templateItem = file_get_contents($this->$templDir."imageItem.html");
     $images = $this->getImages();
     foreach ( $images as $item ) {
       $tmp = $templateItem;
-      $html[] = preg_replace("[[image]]", $item, $tmp);
+      $html[] = preg_replace("/\[\[image_path\]\]/", $this->imgDir.$item, $tmp);
     }
     echo implode($html);
   }
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
+<html>
+<head>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/bootstrap.min.js"></script>
+<head>
+<body>
+
 <?php
 include __DIR__."/class/MicroGallery.php";
 $g = new MicroGallery();
 $g->getImages();
-
 ?>
+
+</body>
+</html>
