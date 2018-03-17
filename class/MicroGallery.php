8c914f3da80f4a47b0c361b59bf4593d1db830d2
Tobias Kohlhepp <tk@root-shell.de>
2018-03-17 16:00:28 +0100

    Init

---
+++ class/MicroGallery.php
@@ -0,0 +1,12 @@
+class MicroGallery {
+  public $imgDir = '../img/';
+  public $templdir = '../templates/';
+
+  function getImages() {
+
+  }
+
+  function showGallery() {
+
+  }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include __DIR__."/class/MicroGallery.php";
+$g = new MicroGallery();
+$g->getImages();
+
+?>
